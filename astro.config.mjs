3f0c912bee3aab15edf09d6480088354ac0f6382
Khari Johnson <kj@gvempire.com>
2023-01-21 01:16:00 -0600

    ci(drone): drone ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kharijohnson.dev/",
+  outDir: "dist",
+  publicDir: "public",
   integrations: [
     tailwind({
       config: {
